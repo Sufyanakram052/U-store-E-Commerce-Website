7da0da14a72d2673004b5b0c6d4b7be90ce6fcfe
Sufyan Akram <58602812+Sufyanakram052@users.noreply.github.com>
2022-02-06 19:37:58 +0500

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using finallab;
+namespace finallab.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(tbluser usr)
+        {
+            myDBEntities obj = new myDBEntities();
+            var a = obj.tblusers.Where(l => l.uname.Equals(usr.uname) && l.upassword.Equals(usr.upassword)).ToList();
+            if(a.Count> 0)
+            {
+                Session["uname"] = usr.uname.ToString();
+                Session["upassword"] = usr.upassword.ToString();
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.msg = "Ivaild username and password!...";
+            }
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            if(Session["uname"] == null)
+            {
+                ViewBag.msg = "Login First.......";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using finallab;
+
+namespace finallab.Controllers
+{
+    public class HomeController : Controller
+    {
+        myDBEntities db = new myDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+           
+            var p = db.tblproes.ToList();
+            ViewBag.p = p;
+
+            var imgs = db.tblimages.ToList();
+            ViewBag.imgs = imgs;
+
+            return View();
+        }
+        public ActionResult cat(int id)
+        {
+            var p = db.tblproes.Where(l => l.cid == id).ToList();
+            ViewBag.p = p;
+            var imgs = db.tblimages.ToList();
+            ViewBag.imgs = imgs;
+            return View();
+        }
+
+        public ActionResult pro(int id)
+        {
+            var p = db.tblproes.Where(l => l.pid == id).ToList();
+            ViewBag.p = p;
+            var imgs = db.tblimages.Where(l => l.pid ==id ).ToList();
+            ViewBag.imgs = imgs;
+            return View();
+        
+        }
+    }
+}
